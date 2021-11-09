62c01fde0fab84d687f1a03b27df289794f71815
SathviQueSam69 <87301015+SathviQueSam69@users.noreply.github.com>
2021-11-09 17:01:33 +0530

    Create Comments.java

---
+++ Comments.java
@@ -0,0 +1,19 @@
+public class Comments {
+    public static void main(String[] args) {
+        // This is the single comments
+        /*
+         * This
+         *  is 
+         * the 
+         * multi Line comments
+         */
+
+        /**
+         * This 
+         * is the 
+         * documentation 
+         * comments
+         */
+
+    }
+}
