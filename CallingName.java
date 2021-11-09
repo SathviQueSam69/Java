eb1a06b2813a9010faa29942da9e2c8c4602684c
SathviQueSam69 <87301015+SathviQueSam69@users.noreply.github.com>
2021-11-09 17:05:50 +0530

    Create CallingName.java

---
+++ CallingName.java
@@ -0,0 +1,11 @@
+public class CallingName {
+    public static void main(String[] args) {
+        Name();
+    }
+
+    public static void Name() {
+        System.out.println("Sathvik");
+
+    }
+
+}
