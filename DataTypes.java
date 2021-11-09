4ebc29f58d867f211191839d45e8dc9dcefaf29d
SathviQueSam69 <sathviksabgipagi8073@gmail.com>
2021-11-09 12:53:29 +0530

    one file is added

---
+++ DataTypes.java
@@ -0,0 +1,17 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int i = 10;
+        float f = 234.5f;
+        char c = 'A';
+        boolean b = true;
+        double d = 6.99;
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(b);
+        System.out.println(d);
+        System.out.println(c);
+        
+    }
+
+}
+
