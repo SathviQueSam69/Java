70561d3b08fdc869c7ae0b0d5f03bc3302f0a523
SathviQueSam69 <87301015+SathviQueSam69@users.noreply.github.com>
2021-11-09 17:15:15 +0530

    Create ClassObjectMethod.java

---
+++ ClassObjectMethod.java
@@ -0,0 +1,12 @@
+public class ClassObjectMethod {
+    int id = 88;
+    String name = "Aruna";
+
+    public static void main(String[] args) {
+
+        ClassObjectMethod O = new ClassObjectMethod();
+        System.out.println(O.id);
+        System.out.println(O.name);
+    }
+
+}
